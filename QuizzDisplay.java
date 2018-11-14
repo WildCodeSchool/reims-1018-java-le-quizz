c01c44709a660086600be22794e37d470c7ba7c9
Anabelle Baynard <baynardanabelle@gmail.com>
2018-11-14 15:45:36 +0100

    règles du jeu en anglais

--- QuizzDisplay.java
+++ QuizzDisplay.java
@@ -1,11 +1,11 @@
 class QuizzDisplay{
   public static void homePage (){
-    System.out.println("Qui veut gagner des Gallions ?");
-    System.out.println("Pour commencer à jouer, entrez d'abord votre pseudo. Les questions vont apparaître au fur et à mesure. Des réponses vous seront proposées, choisissez la bonne! Si vous hésitez, vous pouvez toujours faire appel à un des 3 jockers que vous ne pourrez utiliser qu'une seule fois.");
-    System.out.println("Le Felix Felicis: Vous avez de la chance: il élimine deux mauvaises réponses.");
-    System.out.println("Le chicaneur: Il vous proposera la réponse qu'il pense être la bonne... mais ne serait-ce pas une rumeur...");
-    System.out.println("Le vif d'or: Attrappez-le et vous gagnez! Passez à la question suivante. Ne l'attrappez pas n'importe quand!");
-    System.out.println("A chaque fois que vous obtenez la bonne réponse, vous gagnez un gallion et vous passez à la question suivante. A n'importe quel moment vous pouvez sortir du jeu et partir avec les gallions que vous avez obtenus. Le jeu prendra fin lorsque vous aurez obtenu 15 gallions (soit 7 395 noises!!)");
-    System.out.println("Bonne chance!");
+    System.out.println("Who wants to win Galleons ?");
+    System.out.println("To start playing, first enter your nickname. The questions will appear as and when. Answers will be offered, choose the right one! If you hesitate, you can always use one of the 3 jockers that you can only use once.");
+    System.out.println("Felix Felicis : You're lucky: it eliminates two wrong answers.");
+    System.out.println("The chicaneur: He will suggest the answer he thinks is the right one ... but would not it be a rumor ...");
+    System.out.println("The golden snitch: Catch it and you win! Go to the next question. Do not catch it anytime!");
+    System.out.println("Whenever you get the right answer, you win one gallion and move on to the next question. At any time you can get out of the game and go with the galleys you got. The game will end when you get 15 galleons (7,395 noises !!)");
+    System.out.println("Good luck!");
   }
 }
