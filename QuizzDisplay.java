3a9e0d8d2ff0b89c8caa20e4310c989fb15f7873
Mohamed Kaba <muhamad.kaba@gmail.com>
2018-11-14 15:56:07 +0100

    integration des codes relatifs au lancement de la partie, méthodes scanner etc...

--- QuizzDisplay.java
+++ QuizzDisplay.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+import java.util.*;
+
 class QuizzDisplay{
   public static void homePage (){
     System.out.println("Hello World! It's time to play!");
-  }
+    }
+  public static void displayLaunch (){
+    System.out.println("Enter 'play' to start the quizz");
+    Scanner sc = new Scanner (System.in);
+    String str = sc.nextLine();
+    System.out.println("Here we go!");
+    }
 }
