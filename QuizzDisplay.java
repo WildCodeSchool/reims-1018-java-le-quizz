2860977cd34f6597bae502a1fdd48131a8505312
Anabelle Baynard <baynardanabelle@gmail.com>
2018-11-14 10:52:23 +0100

    écriture des règles du jeu

--- QuizzDisplay.java
+++ QuizzDisplay.java
@@ -1,5 +1,11 @@
 class QuizzDisplay{
   public static void homePage (){
-    System.out.println("Hello World! It's time to play!");
+    System.out.println("Qui veut gagner des Gallions ?");
+    System.out.println("Pour commencer à jouer, entrez d'abord votre pseudo. Les questions vont apparaître au fur et à mesure. Des réponses vont apparaître au fur et à mesure. Des réponses vous seront proposées, choisissez la bonne! Si vous hésitez, vous pouvez toujours faire appel à un des 3 jockers que vous ne pourrez utiliser qu'une seule fois.");
+    System.out.println("Le Felix Felicis: Vous avez de la chance: il élimine deux mauvaises réponses.");
+    System.out.println("Le chicaneur: Il vous proposera la réponse qu'il pense être la bonne... mais ne serait-ce pas une rumeur...");
+    System.out.println("Le vif d'or: Attrappez-le et vous gagnez! Passez à la question suivante. Ne l'attrappez pas n'importe quand!");
+    System.out.println("A chaque fois que vous obtenez la bonne réponse, vous gagnez un gallion et vous passez à la question suivante. A n'importe quel moment vous pouvez sortir du jeu et partir avec les gallions que vous avez obtenus. Le jeu prendra fin lorsque vous aurez obtenu 15 gallions (soit 7 395 noises!!)");
+    System.out.println("Bonne chance!");
   }
 }
