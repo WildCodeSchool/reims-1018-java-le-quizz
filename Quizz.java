ac66c1a0de6cab945593836e6bae563f79dd124b
Anabelle Baynard <baynardanabelle@gmail.com>
2018-11-15 09:48:28 +0100

    merge des fichier et compilation des règles du jeu, de l'entrée du prénom et du play pour jouer

--- Quizz.java
+++ Quizz.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+
 class Quizz {
+
   public static void main (String[] args) {
-      QuizzDisplay.homePage();
+    QuizzDisplay.homePage();
+    String nickname = QuizzDisplay.askForNickname();
+    nickname = "joueur/joueuse " + nickname;
+    QuizzDisplay.displayNickname(nickname);
+    QuizzDisplay.displayLaunch();
   }
 }
